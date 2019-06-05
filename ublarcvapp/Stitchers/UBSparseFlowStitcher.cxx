de3e7b29b2a9fe539f53c5d8f355c1326e5c98b8
twongjirad <taritree.wongjirad@gmail.com>
2019-06-05 08:28:09 -0400

    basic stitcher created

--- ublarcvapp/Stitchers/UBSparseFlowStitcher.cxx
+++ ublarcvapp/Stitchers/UBSparseFlowStitcher.cxx
@@ -3,6 +3,7 @@
 namespace ublarcvapp {
 
   UBSparseFlowStitcher::UBSparseFlowStitcher( const std::vector<larcv::Image2D>& embedding_images )
+    : larcv_base("UBSparseFlowStitcher")
   {
     // We create two images. One to hold vaues, another to hold a metric
     for ( auto const& img : embedding_images ) {
@@ -20,11 +21,19 @@ namespace ublarcvapp {
   {
   }
 
+  void UBSparseFlowStitcher::clear() {
+    _outimg_v.clear();
+    _metric_v.clear();
+  }
 
-  void UBSparseFlowStitcher::addSparseData( const larcv::SparseImage& sparse ) {
+  void UBSparseFlowStitcher::addSparseData( const larcv::SparseImage& sparse,
+                                            const larcv::ImageMeta& target1_subimg,
+                                            const larcv::ImageMeta& target2_subimg ) {
+
     // loop through features
     unsigned int nfeats = sparse.nfeatures();
-    unsigned int nvals  = nfeats-2;
+    LARCV_NORMAL() << "number of features: " << nfeats << std::endl;
+    unsigned int nvals  = nfeats+2;
     const std::vector<float>& pixlist = sparse.pixellist();
 
     // calc mean of meta (for metric)
@@ -34,28 +43,45 @@ namespace ublarcvapp {
     }
       
     
-    for ( size_t ipt=0; ipt<sparse.pixellist().size()/nfeats; ipt++ ) {
-      int row = pixlist.at( ipt*nfeats + 0 );
-      int col = pixlist.at( ipt*nfeats + 1 ); 
-      for ( size_t ifeat=0; ifeat<nvals; ifeat++ ) {
+    for ( size_t ipt=0; ipt<sparse.pixellist().size()/nvals; ipt++ ) {
+      int row = pixlist.at( ipt*nvals + 0 );
+      int col = pixlist.at( ipt*nvals + 1 ); 
+      for ( size_t ifeat=0; ifeat<nfeats; ifeat++ ) {
         const larcv::ImageMeta& meta = sparse.meta(ifeat);
-        float wire = meta.pos_x(col);
-        float tick = meta.pos_y(row);
+        float src_wire = meta.pos_x(col); // source wire
+        float tick     = meta.pos_y(row);
         float met  = fabs((float)col-coldist_v[ifeat]);
         int outrow,outcol;
         try {
           outrow = _outimg_v[ifeat].meta().row( tick );
-          outcol = _outimg_v[ifeat].meta().col( wire );
+          outcol = _outimg_v[ifeat].meta().col( src_wire );
         }
         catch (std::exception& e) {
+          LARCV_WARNING() << "Error filling pt["  << ipt << "] (" << src_wire << ", " << tick << ")" << std::endl;
           continue;
         }
 
         float current_metric = _metric_v[ifeat].pixel( outrow, outcol );
         if ( current_metric<0 || met<current_metric ) {
-          // update metric image
-          _outimg_v[ifeat].set_pixel( outrow, outcol, pixlist.at(ipt*nfeats+2+ifeat) );
-          _metric_v[ifeat].set_pixel( outrow, outcol, met );
+          // update metric image and output value
+          float flowout = pixlist.at(ipt*nvals+2+ifeat);
+          if ( flowout>-1000.0 ) {
+            const larcv::ImageMeta& target_meta = _outimg_v.at(ifeat).meta();
+            float tar_subimg_col  = col + flowout;
+            float tar_subimg_wire = 0;
+            if ( ifeat==0 )
+              tar_subimg_wire = target1_subimg.min_x() + tar_subimg_col*target1_subimg.pixel_width();
+            else
+              tar_subimg_wire = target2_subimg.min_x() + tar_subimg_col*target2_subimg.pixel_width();
+            try {
+              flowout = (float)_outimg_v[ifeat].meta().col( tar_subimg_wire ) - (float)outcol;
+              _outimg_v[ifeat].set_pixel( outrow, outcol, flowout );
+              _metric_v[ifeat].set_pixel( outrow, outcol, met );
+            }
+            catch (std::exception& e) {
+              LARCV_WARNING() << "flow out of wholeview target img" << std::endl;
+            }
+          }
         }
         
       }//end of loop over pixel features
--- ublarcvapp/Stitchers/UBSparseFlowStitcher.h
+++ ublarcvapp/Stitchers/UBSparseFlowStitcher.h
@@ -2,18 +2,23 @@
 #define __UB_SPARSE_FLOW_STITCHER_H__
 
 #include <vector>
+#include "larcv/core/Base/larcv_base.h"
 #include "larcv/core/DataFormat/Image2D.h"
 #include "larcv/core/DataFormat/SparseImage.h"
 
 namespace ublarcvapp {
 
-  class UBSparseFlowStitcher {
+  class UBSparseFlowStitcher : public larcv::larcv_base {
   public:
 
     UBSparseFlowStitcher( const std::vector<larcv::Image2D>& embedding_images );
     ~UBSparseFlowStitcher();
     
-    void addSparseData( const larcv::SparseImage& data );
+    void addSparseData( const larcv::SparseImage& data,
+                        const larcv::ImageMeta& tar1_subimg,
+                        const larcv::ImageMeta& tar2_subimg );
+                        
+    void clear();
 
     std::vector<larcv::Image2D> _outimg_v;
     std::vector<larcv::Image2D> _metric_v;
