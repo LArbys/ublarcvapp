fd9d50611b494514c7267ab804a7fc0680754969
twongjirad <taritree.wongjirad@gmail.com>
2019-09-10 21:05:36 -0400

    cleaned up cropmaskcombo. missing image inserted into crops_v. plan to deprecate missing_v.

--- ublarcvapp/DLTagger/mrcnnmatch/CropMaskCombo.h
+++ ublarcvapp/DLTagger/mrcnnmatch/CropMaskCombo.h
@@ -30,25 +30,26 @@ namespace dltagger {
                    const std::vector<larcv::Image2D>& adc,
                    const std::vector<larcv::Image2D>& wholeview_badch_v,
                    float pixel_threshold=10.0,
-                   int tick_padding=6, int downsample_factor=8 );
+                   int tick_padding=6,
+                   int downsample_factor=8 );
     
     virtual ~CropMaskCombo() {};
     
     std::vector<larcv::Image2D> crops_v;
     std::vector<larcv::Image2D> mask_v;
-    std::vector<larcv::Image2D> missing_v; ///< adds non-empty image for plane that is missing in crops_v and mask_v
+    std::vector<larcv::Image2D> missing_v; ///< [deprecated] adds non-empty image for plane that is missing in crops_v and mask_v
     std::vector<larcv::Image2D> badch_v;   ///< crop from bad channel image
-    int ngoodplanes;
-    int badplane;
     const MaskCombo& getCombo() const { return *_pcombo; };
-
+    bool istwoplane() const { return _twoplane_mode; };
+    int  badPlane() const   { return _badplane; };
     
   protected:
     
     const MaskCombo* _pcombo;
     void _crop_and_mask_image( const std::vector<larcv::Image2D>& wholeview_v );
     void _make_missing_crop( const std::vector<larcv::Image2D>& wholeview_v,
-                             std::vector<larcv::Image2D>& out_v );
+                             std::vector<larcv::Image2D>& out_v,
+                             std::vector<larcv::Image2D>& miss_out_v );
     void _prep_badch_crop( const std::vector<larcv::Image2D>& whole_badch_v,
                            const std::vector<larcv::Image2D>& crop_v,
                            const std::vector<larcv::Image2D>& missing_v,
@@ -59,7 +60,8 @@ namespace dltagger {
     float _threshold;         //< pixel value threshold
     int   _tick_padding;      //< padding added to tick dimensions
     int   _downsample_factor; //< crops will be a multple of this factor in order to allow for downsampling
-
+    bool  _twoplane_mode;     //< if true, one plane crop is inferred from the two other crops
+    int   _badplane;          //< index of inferred missing plane
     
   };
   
