8f3807a46ad2c837783bca9caa3c082bcc2f7c79
twongjirad <taritree.wongjirad@gmail.com>
2019-09-27 13:54:44 -0400

    added empty template for ssnet overlay cheater module

---
+++ ublarcvapp/DLTagger/DLSSNetOverlayCheater.cxx
@@ -0,0 +1,29 @@
+#include "DLSSNetOverlayCheater.h"
+
+namespace ublarcvapp {
+namespace dltagger {
+
+  static DLSSNetOverlayCheaterFactory __global_DLSSNetOverlayCheaterFactory__;
+  
+  void DLSSNetOverlayCheater::configure( const larcv::PSet& pset ) {
+    
+  }
+
+  void DLSSNetOverlayCheater::initialize() {
+  }
+
+  
+  bool DLSSNetOverlayCheater::process( larcv::IOManager& mgr ) {
+    return true;
+  }
+
+  
+  void DLSSNetOverlayCheater::finalize() {
+
+  }
+
+
+  
+  
+}
+}
