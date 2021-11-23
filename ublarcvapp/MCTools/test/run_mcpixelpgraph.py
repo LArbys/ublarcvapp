1b89a38398c4caaa58e89ea704676e638c87844b
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2021-11-23 11:36:41 -0500

    allow one to run the mcpixelpgraph with larlite only

--- ublarcvapp/MCTools/test/run_mcpixelpgraph.py
+++ ublarcvapp/MCTools/test/run_mcpixelpgraph.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 import os,sys,argparse
 
 parser = argparse.ArgumentParser("Test MCPixelPGraph")
 parser.add_argument("-ill", "--input-larlite",required=True,type=str,help="Input larlite file")
-parser.add_argument("-ilcv","--input-larcv",required=True,type=str,help="Input LArCV file")
+parser.add_argument("-ilcv","--input-larcv",required=False,default=None,type=str,help="Input LArCV file")
 parser.add_argument("-adc", "--adc",type=str,default="wire",help="Name of tree with Wire ADC values [default: wire]")
 parser.add_argument("-tb",  "--tick-backward",action='store_true',default=False,help="Input LArCV data is tick-backward [default: false]")
 parser.add_argument("-d",   "--debug", action='store_true', default=False, help="Run in debug mode")
@@ -23,19 +24,24 @@ ioll = larlite.storage_manager( larlite.storage_manager.kREAD )
 ioll.add_in_filename(  args.input_larlite )
 ioll.open()
 
-if args.tick_backward:
-    iolcv = larcv.IOManager( larcv.IOManager.kREAD, "larcv", larcv.IOManager.kTickBackward )
+if args.input_larcv is not None:
+    # has larcv images to get pixels
+    HAS_LARCV = True
+    if args.tick_backward:
+        iolcv = larcv.IOManager( larcv.IOManager.kREAD, "larcv", larcv.IOManager.kTickBackward )
+    else:
+        iolcv = larcv.IOManager( larcv.IOManager.kREAD, "larcv", larcv.IOManager.kTickForward )
+    iolcv.add_in_file( args.input_larcv )
+    iolcv.reverse_all_products()
+    iolcv.initialize()
 else:
-    iolcv = larcv.IOManager( larcv.IOManager.kREAD, "larcv", larcv.IOManager.kTickForward )
-iolcv.add_in_file( args.input_larcv )
-iolcv.reverse_all_products()
-iolcv.initialize()
+    HAS_LARCV = False
 
-nentries = iolcv.get_n_entries()
-print "Number of entries: ",nentries
+nentries = ioll.get_entries()
+print("Number of entries: ",nentries)
 #nentries = 10
 
-print "Start loop."
+print("Start loop.")
 
 mcpg = ublarcvapp.mctools.MCPixelPGraph()
 if args.debug:
@@ -49,29 +55,33 @@ tmp = rt.TFile("temp.root","recreate")
 c = rt.TCanvas("c","c",1200,1800)
 c.Divide(1,3)
 
-for ientry in xrange( nentries ):
+for ientry in range( nentries ):
 
-    print 
-    print "=========================="
-    print "===[ EVENT ",ientry," ]==="
+    print() 
+    print("==========================")
+    print("===[ EVENT ",ientry," ]===")
     ioll.go_to(ientry)
-    iolcv.read_entry(ientry)
-
-    ev_adc = iolcv.get_data( larcv.kProductImage2D, args.adc )
-    print "number of images: ",ev_adc.Image2DArray().size()
-    adc_v = ev_adc.Image2DArray()
-    for p in xrange(adc_v.size()):
-        print " image[",p,"] ",adc_v[p].meta().dump()
+    if HAS_LARCV:
+        iolcv.read_entry(ientry)
+        ev_adc = iolcv.get_data( larcv.kProductImage2D, args.adc )
+        print("number of images: ",ev_adc.Image2DArray().size())
+        adc_v = ev_adc.Image2DArray()
+        for p in range(adc_v.size()):
+            print(" image[",p,"] ",adc_v[p].meta().dump())
     
-    # make histogram
-    hist_v = larcv.rootutils.as_th2d_v( adc_v, "hentry%d"%(ientry) )
-    for ih in xrange(adc_v.size()):
-        h = hist_v[ih]
-        h.GetZaxis().SetRangeUser(0,100)
+        # make histogram
+        hist_v = larcv.rootutils.as_th2d_v( adc_v, "hentry%d"%(ientry) )
+        for ih in range(adc_v.size()):
+            h = hist_v[ih]
+            h.GetZaxis().SetRangeUser(0,100)
 
-    mcpg.buildgraph( iolcv, ioll )
+        mcpg.buildgraph( iolcv, ioll )
+    else:
+        mcpg.buildgraphonly( ioll )
+        
     mcpg.printAllNodeInfo()
     #mcpg.printGraph()
+    sys.exit(0)
 
     #primaries = mcpg.getPrimaryParticles()
     primaries = mcpg.node_v
@@ -79,14 +89,14 @@ for ientry in xrange( nentries ):
     # get primary electron, make tgraph of pixels
     graph_v = []
     bbox_v  = []
-    for i in xrange(primaries.size()):
+    for i in range(primaries.size()):
         node = primaries.at(i)
-        print "primary pid[",node.pid,"]"
+        print("primary pid[",node.pid,"]")
         if node.pid in [11,2212,13,-13,22,211,-211]:
-            print "  making tgraph for pid=",node.pid
+            print("  making tgraph for pid=",node.pid)
             e_v = []
             bb_v = []
-            for p in xrange(3):
+            for p in range(3):
                 if node.pix_vv[p].size()==0:
                     e_v.append(None)
                     bb_v.append(None)
@@ -100,7 +110,7 @@ for ientry in xrange( nentries ):
                 bb.SetFillStyle(0)
                 bb.SetLineWidth(2)
                 
-                for j in xrange( node.pix_vv[p].size()/2 ):
+                for j in range( node.pix_vv[p].size()/2 ):
                     g.SetPoint(j, node.pix_vv[p][2*j+1], node.pix_vv[p][2*j] ) # wire, tick
                 g.SetMarkerStyle(20)
                 g.SetMarkerSize(0.5)                
@@ -130,10 +140,10 @@ for ientry in xrange( nentries ):
             graph_v.append(e_v)
             bbox_v.append(bb_v)
 
-    print "num graphs: ",len(graph_v)
+    print("num graphs: ",len(graph_v))
     
     #draw canvas
-    for p in xrange(3):
+    for p in range(3):
         c.cd(p+1)
         hist_v[p].Draw("colz")
         for e_v in graph_v:
@@ -144,8 +154,8 @@ for ientry in xrange( nentries ):
                 bb_v[p].Draw()
     c.Update()
 
-    print "[enter to continue]"
+    print("[enter to continue]")
     raw_input()    
 
 
-print "=== FIN =="
+print("=== FIN ==")
