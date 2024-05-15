9476bc3f34688138f28dd66fa2a8d072cf55dd77
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2024-05-15 16:07:11 -0400

    finished simple interface to the ubphotonlib data. confirmed visually that it seems to work

--- ublarcvapp/UBPhotonLib/UBPhotonLib.cxx
+++ ublarcvapp/UBPhotonLib/UBPhotonLib.cxx
@@ -1,6 +1,7 @@
 #include "UBPhotonLib.h"
 
 #include <iostream>
+#include <chrono>
 
 #include "TFile.h"
 #include "TTree.h"
@@ -16,7 +17,10 @@ namespace ubphotonlib {
       _cryo_length_cm{383.22,383.22,1221.75},
       _nvoxels_dim{75,75,400},
       _voxel_len_cm{0,0,0},
-      _cryo_origin_tpc_coord_cm{0,0,0}
+      _cryo_origin_tpc_coord_cm{0,0,0},
+      _nopchs(32),
+      _nvoxels(0),
+      _nvis(0)
   {
     
     std::cout << "[UBPhotonLib] Loaded" << std::endl;
@@ -26,7 +30,18 @@ namespace ubphotonlib {
       _cryo_origin_tpc_coord_cm[i] = _cryo_global_origin_cm[i] - _tpc_global_origin_cm[i];
     }
     std::cout << "  Voxel Length per dim [" << _voxel_len_cm[0] << "," << _voxel_len_cm[1] << "," << _voxel_len_cm[2] << "]" << std::endl;
+    std::cout << "  Cryostat voxel grid origin in TPC coords: ("
+	      << _cryo_origin_tpc_coord_cm[0] << ", "
+	      << _cryo_origin_tpc_coord_cm[1] << ", " 
+	      << _cryo_origin_tpc_coord_cm[2] << ")"
+	      << std::endl;
+    std::cout << "  Cryostat Max Coordinates: ("
+	      << _cryo_origin_tpc_coord_cm[0]+_cryo_length_cm[0] << ", "
+	      << _cryo_origin_tpc_coord_cm[1]+_cryo_length_cm[1] << ", "
+	      << _cryo_origin_tpc_coord_cm[2]+_cryo_length_cm[2] << ")"
+	      << std::endl;
 
+    
 
     std::string dat_folder = std::getenv("UBLARCVAPP_BASEDIR");
     std::string photon_vis_path = dat_folder + "/ublarcvapp/UBPhotonLib/dat/uboone_photon_library_v6_70kV_EnhancedExtraTPCVis.root";
@@ -41,14 +56,55 @@ namespace ubphotonlib {
     vistree->SetBranchAddress("Voxel",&Voxel);
     vistree->SetBranchAddress("OpChannel",&OpChannel);
     vistree->SetBranchAddress("Visibility",&Visibility);
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    
+    //_voxelopchindex_v.resize(nentries);
+    std::cout << "   Allocating memory for " << _nvoxels_dim[0] << "x" << _nvoxels_dim[1] << "x" << _nvoxels_dim[2] << "x" << _nopchs << std::endl;
+    _nvoxels = _nvoxels_dim[0]*_nvoxels_dim[1]*_nvoxels_dim[2];
+    _nvis = _nvoxels*_nopchs;
+    _visibility_v.resize( _nvis, 0.0 );
+
+    std::vector<float> vcoord(3,0);    
+    long lindex = 0;
     
     for (size_t i=0; i<nentries; i++) {
       vistree->GetEntry(i);
-      std::pair<long,int> voxelkey( (long)Voxel, (int)OpChannel );
-      _voxelopchindex_to_visibility[ voxelkey ] = Visibility;
-    }
+      //std::pair<long,int> voxelkey( (long)Voxel, (int)OpChannel );
+      //_voxelopchindex_to_visibility[ voxelkey ] = Visibility;
+      //_voxelopchindex_v[i].voxelid = (long)Voxel;
+      //_voxelopchindex_v[i].opchid  = (int)OpChannel;
+      //_voxelopchindex_v[i].vis     = Visibility;
+      //_voxel_to_vindex_v[ (long)Voxel ] = (long)i;
+      // need to slice the Voxel
 
-    std::cout << "  Loaded Visibility Info" << std::endl;
+      // copied from getvoxelcoords to maybe trigger vectorization
+      //std::vector<long> voxcoord = getVoxelCoords( Voxel );      
+      // vcoord[0] = voxelid % _nvoxels_dim[0];
+      // long ivox = voxelid-(long)vcoord[0];    
+      // vcoord[1] = ((ivox)/_nvoxels_dim[0]) % _nvoxels_dim[1];
+      // vcoord[2] = ((ivox - vcoord[1]*_nvoxels_dim[0])/(_nvoxels_dim[1]*_nvoxels_dim[0])) % _nvoxels_dim[2];
+      
+      //long lindex = getVisLibIndex( Voxel, OpChannel );
+      // opchannel is almost certaintly what UB calls "OpDet"      
+      lindex = Voxel*_nopchs + OpChannel; 
+      
+      if ( i>0 && i%10000000==0 ) {
+	std::cout << "   loading entry " << i  << ":"
+		  << " vis=" << Visibility
+		  << " vox=" << Voxel
+		  << " opdet=" << OpChannel
+		  <<"  lindex=" << lindex
+		  << std::endl;
+      }
+      _visibility_v[lindex] = Visibility;
+    }
+    
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> ms_double = t2 - t1;
+    double sec = ms_double.count()*0.001;
+    std::cout << "  Loaded Visibility Info in " << sec << " secs"  << std::endl;
+    
   }
 
   UBPhotonLib::~UBPhotonLib()
@@ -70,53 +126,77 @@ namespace ubphotonlib {
     _ginstance = nullptr;
   }
 
-  std::vector<int> UBPhotonLib::getVoxelDimIndices( const std::vector<float>& pos )
+  std::vector<long> UBPhotonLib::getVoxelCoords( const std::vector<float>& pos ) const
   {
-    std::vector<int> iindex(3,0);
+    std::vector<long> vcoord(3,0);
     for (int i=0; i<3; i++) {
-      iindex[i] = (int)((pos[i] - _cryo_origin_tpc_coord_cm[i])/_voxel_len_cm[i]);
+      vcoord[i] = (long) (pos[i] - _cryo_origin_tpc_coord_cm[i])/_voxel_len_cm[i];
     }
-    return iindex;
+    return vcoord;
+  }
+    
+  std::vector<long> UBPhotonLib::getVoxelCoords( long voxelid ) const
+  {
+    // taken from larsim/Simulation/PhotonVoxels.cxx
+    // std::vector<int> ReturnVector;
+    // ReturnVector.resize(3);
+    // ReturnVector.at(0) =  ID % fxSteps ;
+    // ReturnVector.at(1) =  ((ID - ReturnVector.at(0) ) / fxSteps) % fySteps ;
+    // ReturnVector.at(2) =  ((ID - ReturnVector.at(0) - (ReturnVector.at(1) * fxSteps)) / (fySteps * fxSteps)) % fzSteps ;
+    // return ReturnVector;
+
+    std::vector<long> vcoord(3,0);
+
+    vcoord[0] = voxelid % _nvoxels_dim[0];
+    long ivox = voxelid-(long)vcoord[0];    
+    vcoord[1] = ((ivox)/_nvoxels_dim[0]) % _nvoxels_dim[1];
+    vcoord[2] = ((ivox - vcoord[1]*_nvoxels_dim[0])/(_nvoxels_dim[1]*_nvoxels_dim[0])) % _nvoxels_dim[2];
+    return vcoord;
   }
 
-  long UBPhotonLib::getVoxelIndex( const std::vector<float>& pos )
+  long UBPhotonLib::getVisLibIndex( long voxelid, int opch ) const
   {
-    std::vector<int> iindex = getVoxelDimIndices( pos );
+    return voxelid*_nopchs + opch;
+  }
+
+  long UBPhotonLib::getVoxelID( const std::vector<long>& voxcoords ) const
+  {
     bool valid = true;
     for (int i=0; i<3; i++) {
-      if ( iindex[i]<0 || iindex[i]>=_nvoxels_dim[i] )
+      if ( voxcoords[i]<0 || voxcoords[i]>=_nvoxels_dim[i] )
 	valid = false;
     }
-
     if (!valid)
       return -1;
+    return voxcoords[0] + voxcoords[1]*_nvoxels_dim[0] + voxcoords[2]*_nvoxels_dim[1]*_nvoxels_dim[0];
+  }
+  
+  long UBPhotonLib::getVisLibIndex( const std::vector<float>& pos, int opch ) const
+  {
+    std::vector<long> iindex = getVoxelCoords( pos );
+    long voxelid = getVoxelID(iindex);
+    if (voxelid<0)
+      return -1;
     
-    long lindex = iindex[0]*_nvoxels_dim[1]*_nvoxels_dim[2] + iindex[1]*_nvoxels_dim[2] + iindex[2];
+    long lindex = getVisLibIndex( voxelid, opch );
     
     return lindex;
   }
 
   float UBPhotonLib::getVisibility( const std::vector<float>& pos, int opch )
   {
-    long lindex = getVoxelIndex( pos );
-    if ( lindex<0 )
+    long lindex = getVisLibIndex( pos, opch );
+    if ( lindex<0 || lindex>(long)_visibility_v.size())
       return 0.0;
-
-    std::pair<long,int> voxopch_key( lindex, opch );
-    auto it=_voxelopchindex_to_visibility.find( voxopch_key );
-    if ( it==_voxelopchindex_to_visibility.end() ) {
-      // not found
-      return 0.0;
-    }
 
     // return visibility
-    return it->second;
+    return _visibility_v[lindex];
   }
 
   float UBPhotonLib::getVisibilityTrilinear( const std::vector<float>& pos, int opch )
   {
-    long lindex = getVoxelIndex( pos );
-    if ( lindex<0 )
+    long lindex = getVoxelIndex( pos, opch );
+    if ( lindex<0 || lindex>(long)_visibility_v.size() )
       return 0.0;
 
     std::pair<long,int> voxopch_key( lindex, opch );
--- ublarcvapp/UBPhotonLib/UBPhotonLib.h
+++ ublarcvapp/UBPhotonLib/UBPhotonLib.h
@@ -19,25 +19,61 @@ namespace ubphotonlib {
     static UBPhotonLib* getPhotonLib();
     static void destroy(); ///< to free up memory
 
-    std::vector<int> getVoxelDimIndices( const std::vector<float>& pos );
-    long getVoxelIndex( const std::vector<float>& pos );
     float getVisibility( const std::vector<float>& pos, int opch );
-    float getVisibilityTrilinear( const std::vector<float>& pos, int opch );    
+    float getVisibilityTrilinear( const std::vector<float>& pos, int opch );
+
+    std::vector<long> getVoxelCoords( long voxelid ) const;
+    std::vector<long> getVoxelCoords( const std::vector<float>& pos ) const;    
+    long getVisLibIndex( long voxelid, int opch ) const;
+    long getVoxelIndex( const std::vector<float>& pos, int opch ) const;
+    long getVisLibIndex( const std::vector<float>& pos, int opch ) const;
+    long getVoxelID( const std::vector<long>& voxcoords ) const;
+
+    struct VisData_t {
+      long voxelid;
+      long opchid;
+      int dim_index[3];
+      float vis;
+      VisData_t()
+	: voxelid(0),
+	  opchid(0),
+	  dim_index{0,0,0},
+	  vis(0.0)
+      {};
+      bool operator<( VisData_t& rhs ) const {
+	if (rhs.voxelid < voxelid )
+	  return true;
+	if ( rhs.voxelid==voxelid && rhs.opchid < opchid )
+	  return true;
+	return false;
+      };
+    };
     
   protected:
 
     UBPhotonLib();
     virtual ~UBPhotonLib();
 
-
+    
+    
     float _tpc_global_origin_cm[3];
     float _cryo_global_origin_cm[3];
     float _cryo_length_cm[3];    
     int _nvoxels_dim[3];
     float _voxel_len_cm[3];
-    float _cryo_origin_tpc_coord_cm[3];    
+    float _cryo_origin_tpc_coord_cm[3];
+    int _nopchs;
+    long _nvoxels;
+    long _nvis;
 
     std::map< std::pair<long,int> , float > _voxelopchindex_to_visibility;
+    std::vector< VisData_t > _voxelopchindex_v;
+    std::map< long, long > _voxel_to_vindex_v;
+
+    // fastest look up is a 4D matrix    
+    // we'll use a 1D vector and execute strides
+    // will be a lot of zeros. But it's only double the memory.
+    std::vector<float> _visibility_v;
     
   private:
 
